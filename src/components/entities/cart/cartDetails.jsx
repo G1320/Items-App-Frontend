7cda3775274957fef2298263e3580f58b75a6f74
Darnell Green <123552242+G1320@users.noreply.github.com>
2024-05-02 15:40:11 +0300

    Cart details now shows up-to-date cart

--- src/components/entities/cart/cartDetails.jsx
+++ src/components/entities/cart/cartDetails.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import CartItemsList from './cartItemsList';
+import { useUserCart } from '../../../hooks/dataFetching/useUserCart';
+import { useUserContext } from '../../../contexts/UserContext';
 
 const CartDetails = () => {
+  const { user } = useUserContext();
+  const { data: cartItems } = useUserCart(user?._id);
+
   return (
     <section className="cart-details">
       <h2>Cart items list</h2>
-      <CartItemsList />
+      <CartItemsList cartItems={cartItems} />
     </section>
   );
 };
